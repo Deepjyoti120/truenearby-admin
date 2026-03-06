3904898997b3d5c216baab1350102698efe61f72
Deepjyoti120 <deepjyoti120281@gmail.com>
2026-03-06 20:35:53 +0530

    done

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import { resolve } from "node:path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-}; 
+  turbopack: {
+    root: resolve(process.cwd(), "../.."),
+  },
+};
 export default nextConfig;
