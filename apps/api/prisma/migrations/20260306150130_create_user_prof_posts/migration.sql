ca74dae1912378e81075e112f834c605c033decf
Deepjyoti120 <deepjyoti120281@gmail.com>
2026-03-21 23:20:24 +0530

    done

--- apps/api/prisma/migrations/20260306150130_create_user_prof_posts/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "posts" ALTER COLUMN "imageUrls" DROP DEFAULT,
-ALTER COLUMN "imageFileIds" DROP DEFAULT;
--- apps/api/prisma/migrations/20260308173702_update_dateof_both_field_property/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX IF EXISTS "profiles_interests_gin_idx";
-
--- AlterTable
-ALTER TABLE "profiles" ALTER COLUMN "birthDate" SET DATA TYPE DATE;
