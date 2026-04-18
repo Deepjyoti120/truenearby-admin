4fbc947bc81fb673e3e5f0b40839cad18e1f22bc
deep-sumato <deepjyoti.sumatoglobal@gmail.com>
2026-04-18 10:43:59 +0530

    block is also done

--- apps/api/src/matches/matches.service.ts
+++ apps/api/src/matches/matches.service.ts
@@ -10,17 +10,37 @@ export class MatchesService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getMatches(currentUserId: string) {
+    const blocks = await this.prisma.block.findMany({
+      where: {
+        OR: [{ blockerId: currentUserId }, { blockedId: currentUserId }],
+      },
+      select: {
+        blockerId: true,
+        blockedId: true,
+      },
+    });
+
+    const blockedUserIds = blocks.map((block) =>
+      block.blockerId === currentUserId ? block.blockedId : block.blockerId,
+    );
+
     const matches = await this.prisma.match.findMany({
       where: {
         OR: [
           {
             userAId: currentUserId,
+            userBId: {
+              notIn: blockedUserIds,
+            },
             userB: {
               isActive: true,
             },
           },
           {
             userBId: currentUserId,
+            userAId: {
+              notIn: blockedUserIds,
+            },
             userA: {
               isActive: true,
             },
